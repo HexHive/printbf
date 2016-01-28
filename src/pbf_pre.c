e88f38868d4f2a24ac92da2be873d96b35c30e5f
Mathias Payer <mathias.payer@nebelwelt.net>
2016-01-28 16:53:55 -0500

    Reduce needed compiler flags, turn on ALL the warnings

--- src/pbf_pre.c
+++ src/pbf_pre.c
@@ -47,6 +47,8 @@ char bf_BACKWARD_N_fmt2[100];
 char bf_BACKWARD_N_fmt3[100];
 char bf_ADD_N_fmt[100];
 
+char bf_FETCH_fmt[100];
+
 char* syms[] = {
   0,
   bf_LT_fmt, 
@@ -67,7 +69,8 @@ char* syms[] = {
   bf_BACKWARD_N_fmt1, 
   bf_BACKWARD_N_fmt2, 
   bf_BACKWARD_N_fmt3, 
-  bf_ADD_N_fmt
+  bf_ADD_N_fmt,
+  bf_FETCH_fmt
 };
 
 char** real_syms;
@@ -79,33 +82,34 @@ char** real_syms;
  * No cheating here, move along.
  */
 void setup() {
+  unsigned int i;
   inptr = 0;
   ptr = 0;
   pc = 2;
 
   real_syms = (char**)calloc(1,1<<17);
-  real_syms = (void*)real_syms + 0x10000-(((long long int)real_syms)&0xFFFF);
+  real_syms = (char**)((char*)real_syms + 0x10000-(((long)real_syms)&0xFFFF));
 
-  for (int i = 0; i < sizeof(syms)/sizeof(*syms); i++) {
+  for (i = 0; i < sizeof(syms)/sizeof(*syms); i++) {
     real_syms[i] = syms[i];
   }
 
   array_ = malloc(2<<24);
-  array = (void*)array_ + 0x1000000-(((long long int)array_)&0xFFFFFF);
+  array = (void*)((char*)array_ + 0x1000000-(((long)array_)&0xFFFFFF));
   output_ = malloc(2<<24);
-  output = (void*)output_ + 0x1000000-(((long long int)output_)&0xFFFFFF);
+  output = (void*)((char*)output_ + 0x1000000-(((long)output_)&0xFFFFFF));
   input_ = malloc(2<<24);
-  input = (void*)input_ + 0x1000000-(((long long int)input_)&0xFFFFFF);
+  input = (void*)((char*)input_ + 0x1000000-(((long)input_)&0xFFFFFF));
 
-  progn = (void*)progn_ + 0x10000-(((long long int)progn_)&0xFFFF);
+  progn = (void*)((char*)progn_ + 0x10000-(((long)progn_)&0xFFFF));
 
-  for (int i = 0; i < sizeof(array_)/sizeof(*array); i++) {
+  for (i = 0; i < sizeof(array_)/sizeof(*array); i++) {
     array_[i] = 0;
   }
-  for (int i = 0; i < sizeof(progn_)/sizeof(*progn); i++) {
+  for (i = 0; i < sizeof(progn_)/sizeof(*progn); i++) {
     progn_[i] = 0;
   }
-  for (int i = 0; i < sizeof(input_)/sizeof(*input); i++) {
+  for (i = 0; i < sizeof(input_)/sizeof(*input); i++) {
     input_[i] = -1;
   }
 
@@ -134,6 +138,8 @@ void setup() {
 
   strcpy(bf_ADD_N_fmt, "%3$.*3$d%10$.*10$d%4$hhn"); 
 
+  strcpy(bf_FETCH_fmt, "%1$.*1$d%1$.*1$d%1$.*1$d%1$.*1$d%1$.*1$d%1$.*1$d%1$."
+      "*1$d%1$.*1$d%2$hn");
 }
 
 int cond = 0;
@@ -150,16 +156,15 @@ void loop() {
   int* rpc = &progn[pc];
 
 #ifdef NOCHEATING  
-  long long int copyarray = 0;
-  long long int copyoutput = 0;
-  long long int copyinput = 0;
+  long copyarray = 0;
+  long copyoutput = 0;
+  long copyinput = 0;
   int rpc1;
 #endif
 
   while (*rpc != 0) {
-    // fetch -- decode next instruction
-    sprintf(buf, "%1$.*1$d%1$.*1$d%1$.*1$d%1$.*1$d%1$.*1$d%1$.*1$d%1$."
-      "*1$d%1$.*1$d%2$hn", *rpc, (short*)(&real_syms));
+    /* fetch -- decode next instruction */
+    sprintf(buf, bf_FETCH_fmt, *rpc, (short*)(&real_syms));
 
 #ifdef NOCHEATING
     sprintf((void*)(&copyarray), "%s", (char*)(&array));
@@ -168,40 +173,40 @@ void loop() {
     sprintf((void*)(&rpc1), "%s", (char*)(&rpc[1]));
     sprintf((void*)(&rpc1)+1, "%s", (char*)(&rpc[1])+1);
 
-    // execute instruction
+    /* execute instruction */
     sprintf(buf, *real_syms,
-	    copyarray, &array, // 1, 2
-	    *array, array, // 3, 4
-	    output, // 5
-	    copyoutput, &output, // 6, 7
-	    &cond, &bf_CGOTO_fmt3[0], // 8, 9
-	    rpc1, &rpc, // 10, 11
-	    0, //12
-	    *input, // 13
-	    copyinput, &input // 14, 15
+	    copyarray, &array, /* 1, 2 */
+	    *array, array, /* 3, 4 */
+	    output, /* 5 */
+	    copyoutput, &output, /* 6, 7 */
+	    &cond, &bf_CGOTO_fmt3[0], /* 8, 9 */
+	    rpc1, &rpc, /* 10, 11 */
+	    0, /* 12 */
+	    *input, /* 13 */
+	    copyinput, &input /* 14, 15 */
 	    );
 #else
-    // execute instruction
+    /* execute instruction */
     sprintf(buf, *real_syms,
-      // slight cheating for bitwise and
-	    ((long long int)array)&0xFFFF, &array, // 1, 2
-	    *array, array, // 3, 4
-	    output, // 5
-      // slight cheating for bitwise and
-      ((long long int)output)&0xFFFF, &output, // 6, 7
-	    &cond, &bf_CGOTO_fmt3[0], // 8, 9
-	    rpc[1], &rpc, // 10, 11
-	    0, //12
-	    *input, // 13
-      // slight cheating for bitwise and
-	    ((long long int)input)&0xFFFF, &input // 14, 15
+      /* slight cheating for bitwise and */
+	    ((long)array)&0xFFFF, &array, /* 1, 2 */
+	    *array, array, /* 3, 4 */
+	    output, /* 5 */
+      /* slight cheating for bitwise and */
+      ((long)output)&0xFFFF, &output, /* 6, 7 */
+	    &cond, &bf_CGOTO_fmt3[0], /* 8, 9 */
+	    rpc[1], &rpc, /* 10, 11 */
+	    0, /* 12 */
+	    *input, /* 13 */
+      /* slight cheating for bitwise and */
+	    ((long)input)&0xFFFF, &input /* 14, 15 */
 	    );
 #endif
-    // retire -- update PC
+    /* retire -- update PC */
     sprintf(buf, "12345678%.*d%hn", 
-      (int)(((long long int)rpc)&0xFFFF), 0, (short*)&rpc);
+      (int)(((long)rpc)&0xFFFF), 0, (short*)&rpc);
 
-    // for debug: do we need to print?
+    /* for debug: do we need to print? */
     if (output != last) {
       putchar(output[-1]);
       last = output;
@@ -210,18 +215,18 @@ void loop() {
 }
 
 int main() {
+  struct timeval tval_before, tval_after, tval_result;
+
   setup();
 
-  // program
+  /* program */
   ###TOKER###
-
-  struct timeval tval_before, tval_after, tval_result;
 
   gettimeofday(&tval_before, NULL);
 
   strcpy(input, "###INPUT###");
 
-  // execute the bf program
+  /* execute the bf program */
   loop();
 
   gettimeofday(&tval_after, NULL);
@@ -232,4 +237,6 @@ int main() {
     (long int)tval_result.tv_sec,
     (long int)tval_result.tv_usec
     );
+
+  return 0;
 }
